1dd65acc57179278f09bbce65cadee8e39b9c325
Abhilash Asokan <abhilash.asokan@logicline.de>
2019-11-09 14:05:45 +0530

    template setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBZWlSRU4ZT-M7_vleyd_oMlNfocGMEJ9I',
+    authDomain: 'sample-cart.firebaseapp.com',
+    databaseURL: 'https://sample-cart.firebaseio.com',
+    projectId: 'sample-cart',
+    storageBucket: 'sample-cart.appspot.com',
+    messagingSenderId: '159360473196',
+    appId: '1:159360473196:web:5a8cf276dbfa00dffda049',
+    measurementId: 'G-01ZWY2X34Z'
+  }
 };
 
 /*
